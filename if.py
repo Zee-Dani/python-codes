10e3b5b6835e6a5dee0ed9f045f44c96a4c5eee5
Zidani Zipya <zidzipya@gmail.com>
2020-06-22 06:19:32 +0100

    if statement

---
+++ if.py
@@ -0,0 +1,22 @@
+"""n=2
+m=4
+if  n > m :
+    print ('n is greater')
+else:
+    print ('m is greater')"""
+
+score = 156
+
+if score  <= 39 : 
+    print ('failed')
+else:
+    print ('passed')
+
+if (score > 100)  & (score < 200):
+     print ('Genius')
+
+
+    
+
+    
+           
